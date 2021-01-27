e56f80ff649877bbcc3fcdc62491278c71a55c88
Amir <amirborro@gmail.com>
2021-01-27 14:41:08 +0100

    Completed app
    Add fetch, remove and refresh functionnalities

--- 2-tours/setup/src/App.js
+++ 2-tours/setup/src/App.js
@@ -5,6 +5,10 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -27,12 +31,24 @@ function App() {
         <Loading />
       </main>
     );
-  } else
+  }
+  if (tours.length === 0) {
     return (
       <main>
-        <Tours />
+        <div className="title">
+          <h2> No tours left </h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
       </main>
     );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
 export default App;
