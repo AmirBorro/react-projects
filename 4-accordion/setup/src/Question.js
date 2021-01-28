b7cb38b0e918086eb5421c070be883c908ebd815
Amir <amirborro@gmail.com>
2021-01-28 13:39:22 +0100

    completed project
    Accordion completed

--- 4-accordion/setup/src/Question.js
+++ 4-accordion/setup/src/Question.js
@@ -1,7 +1,22 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ title, info }) => {
+  const [isTog, setIsTog] = useState(false);
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        {isTog ? (
+          <AiOutlineMinus className="btn" onClick={() => setIsTog(!isTog)} />
+        ) : (
+          <AiOutlinePlus className="btn" onClick={() => setIsTog(!isTog)} />
+        )}
+      </header>
+      {isTog && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
