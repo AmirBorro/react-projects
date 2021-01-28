afd530df32f5a5ad16b1b7b3ef73f686fc7d2352
Amir <amirborro@gmail.com>
2021-01-28 12:18:28 +0100

    Completed project
    The next and preview buttons have been made differently than on the instructions.

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -1,9 +1,55 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  const numberOfPeeps = people.length - 1;
+  const nextIndex = () => {
+    if (index >= numberOfPeeps) {
+      return setIndex(0);
+    } else {
+      return setIndex(index + 1);
+    }
+  };
+  const prevIndex = () => {
+    if (index <= 0) {
+      return setIndex(numberOfPeeps);
+    } else {
+      return setIndex(index - 1);
+    }
+  };
+  const surpriseMe = () => {
+    const randomPerson = Math.floor(Math.random() * (4 - 1)) + 1;
+    setIndex(randomPerson);
+    console.log(randomPerson);
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="qote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevIndex}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextIndex}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn" onClick={surpriseMe}>
+        Surprise me
+      </button>
+    </article>
+  );
 };
 
 export default Review;
