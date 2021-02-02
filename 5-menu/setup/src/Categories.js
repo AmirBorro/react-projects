74dd3a20ec55882179fdc3621bfd9f1bb6db3146
Amir <amirborro@gmail.com>
2021-02-02 16:27:20 +0100

    Project 5
    Incomplete

--- 5-menu/setup/src/Categories.js
+++ 5-menu/setup/src/Categories.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button className="filter-btn"></button>
+    </div>
+  );
 };
 
 export default Categories;
