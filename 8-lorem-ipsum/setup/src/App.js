54dc6fe2a7550affe82add75027d157b72d563f7
Amir <amirborro@gmail.com>
2021-02-03 12:07:53 +0100

    Lorem Ipsum project completed

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,42 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText(data.slice(0, count));
+  };
+
+  useEffect(() => {
+    if (count < 0) {
+      setCount(0);
+    }
+  }, [count]);
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center ">
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
